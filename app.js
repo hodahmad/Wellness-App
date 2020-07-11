ebb5fbaf6be661dbb059f150d5c205346f351b3b
Hoda Ahmad <ccs7vp@github.com>
2020-07-11 15:03:20 -0400

    Adjusted home page

--- app.js
+++ app.js
@@ -2,11 +2,4 @@
 //API needed to fetch a new quote every single day OR by each page refresh 
 // https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand
 
-// How are you feeling today:
-// 1. If happy emoji is clicked, display text: Awesome! Be sure to continue taking time for yourself.
-
-// 2. If sad emoji is clicked, display text: -QUOTE- then: Check out our free <a href meditation> to uplift your mood!
-
-// 3.) If neutral emoji is clicked, display text: Explore our site to find the perfect <a href recipe> to make, <a href fitness workout> to follow, or <a href meditate>!
-
 console.log("Checking connection");
